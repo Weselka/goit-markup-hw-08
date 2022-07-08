5c783fd3ea2d1fc494d97e2f474791b96e6bf297
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-07-08 23:23:15 +0300

    Adds mobile menu

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,8 @@
   const refs = {
     openMenulBtn: document.querySelector('[data-menu-open]'),
     closeMenulBtn: document.querySelector('[data-menu-close]'),
-    modal: document.querySelector('[data-menu]'),
+    menu: document.querySelector('[data-menu]'),
+    body: document.querySelector('body'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -10,5 +11,6 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
   }
 })();
