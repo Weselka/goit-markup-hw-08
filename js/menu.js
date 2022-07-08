5a022a92c4d32ff11ea36d7c3e7212dbd9919047
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-07-08 06:59:19 +0300

    Adds mobile menu part.1

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
+  const refs = {
+    openMenulBtn: document.querySelector('[data-modal-open]'),
+    closeMenulBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-menu]'),
+  };
 
-    menuBtnRef.addEventListener("click", () => {
-        
-        // const expanded =
-        //     menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-        
-        menuBtnRef.classList.toggle("is-open");
-        // menuBtnRef.setAttribute("aria-expanded", !expanded);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
-        // mobileMenuRef.classList.toggle("is-open");
-    });
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
 })();
